add752942e1363e6590a39bca7ca4d6905ed0690
Анатолий Ведерников <112936302+AnatolyiBooom@users.noreply.github.com>
2023-06-25 13:46:18 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+let accordions = document.querySelectorAll('.questions__faq');
+
+accordions.forEach(el => {
+  el.addEventListener('click', (e) => {
+    let self = e.currentTarget;
+    self.classList.toggle('questions__faq--open');
+  });
+});
+
+
+let body = document.querySelector('body');
+let burger = document.querySelector('.header__menu-btn');
+let nav = document.querySelector('.header__nav');
+let link = document.querySelectorAll('.header__link')
+
+burger.addEventListener('click', function() {
+  burger.classList.toggle('burger-active');
+  nav.classList.toggle('header__nav-active');
+});
+
+link.forEach(el => {
+  el.addEventListener('click', () => {
+  burger?.classList.remove('burger-active');
+  nav?.classList.remove('header__nav-active');
+      });
+  });
+
