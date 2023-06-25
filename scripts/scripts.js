33bcdf11fa7b5df4ef9e7729d9160e4968ce30a9
Анатолий Ведерников <112936302+AnatolyiBooom@users.noreply.github.com>
2023-06-25 14:42:17 +0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,20 +8,20 @@ accordions.forEach(el => {
 });
 
 
-// let body = document.querySelector('body');
-// let burger = document.querySelector('.header__menu-btn');
-// let nav = document.querySelector('.header__nav');
-// let link = document.querySelectorAll('.header__link')
+let body = document.querySelector('body');
+let burger = document.querySelector('.header__menu-btn');
+let nav = document.querySelector('.header__nav');
+let link = document.querySelectorAll('.header__link')
 
-// burger.addEventListener('click', function() {
-//   burger.classList.toggle('burger-active');
-//   nav.classList.toggle('header__nav-active');
-// });
+burger.addEventListener('click', function() {
+  burger.classList.toggle('burger-active');
+  nav.classList.toggle('header__nav-active');
+});
 
-// link.forEach(el => {
-//   el.addEventListener('click', () => {
-//   burger?.classList.remove('burger-active');
-//   nav?.classList.remove('header__nav-active');
-//       });
-//   });
+link.forEach(el => {
+  el.addEventListener('click', () => {
+  burger?.classList.remove('burger-active');
+  nav?.classList.remove('header__nav-active');
+      });
+  });
 
